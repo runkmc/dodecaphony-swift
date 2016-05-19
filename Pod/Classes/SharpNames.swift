9a076976f74b56184fbdb06ca2efb98eefc13da6
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-05-19 14:40:59 -0700

    Add SharpNames enum

---
+++ Pod/Classes/SharpNames.swift
@@ -0,0 +1,24 @@
+//
+//  SharpNames.swift
+//  Pods
+//
+//  Created by Kevin McGladdery on 5/19/16.
+//
+//
+
+import Foundation
+
+enum SharpNames: Int {
+    case A = 1
+    case ASharp
+    case B
+    case C
+    case CSharp
+    case D
+    case DSharp
+    case E
+    case F
+    case FSharp
+    case G
+    case GSharp
+}
